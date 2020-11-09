6629b62ebf9fc5f183fc232239eb660fdaa599e4
sogna <win778500@gmail.com>
2020-11-09 23:34:49 +0900

    Update transform.py

--- code/v1/transform.py
+++ code/v1/transform.py
@@ -63,6 +63,28 @@ def transform_v1(config):
 
     return train_transforms, test_transforms
 
+def transform_v2(config):
+    train_transforms = albumentations.Compose([
+        HorizontalFlip(p=0.5),
+        ImageCompression(quality_lower=99, quality_upper=100),
+        ShiftScaleRotate(shift_limit=0.25, scale_limit=0.25,
+                                        rotate_limit=10, border_mode=0, p=0.7),
+        RandomResizedCrop(config.image_size, config.image_size, p=0.5),
+        Cutout(max_h_size=int(config.image_size * 0.6),
+               max_w_size=int(config.image_size * 0.6), num_holes=1,
+                              p=0.5),
+        Normalize(),
+        ToTensor()
+    ])
+
+    test_transforms = albumentations.Compose([
+        Resize(config.image_size, config.image_size),
+        Normalize(),
+        ToTensor()
+    ])
+
+    return train_transforms, test_transforms
+
 
 def get_transform(config):
     try:
