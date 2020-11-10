07ec4674f797f844d3adda4ebfe107b89f883c3b
sogna <win778500@gmail.com>
2020-11-10 23:59:11 +0900

    Update transform.py

--- code/v1/transform.py
+++ code/v1/transform.py
@@ -87,6 +87,36 @@ def transform_v2(config):
     return train_transforms, test_transforms
 
 
+def transform_v3(config):
+    train_transforms = Compose([
+        Resize(config.image_size, config.image_size),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    test_transforms = Compose([
+        ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
+        GaussNoise(p=1),
+        GaussianBlur(blur_limit=3, p=1),
+        HorizontalFlip(),
+        Resize(config.image_size, config.image_size),
+        OneOf([RandomBrightnessContrast(),
+               FancyPCA(),
+               HueSaturationValue()], p=1),
+        ShiftScaleRotate(shift_limit=0.1,
+                         scale_limit=0.2,
+                         rotate_limit=10,
+                         border_mode=cv2.BORDER_CONSTANT,
+                         p=1),
+        ToTensor(),
+    ])
+
+    return train_transforms, test_transforms
+
+
 def get_transform(config):
     try:
         name = f"transform_v{config.transform_version}"
